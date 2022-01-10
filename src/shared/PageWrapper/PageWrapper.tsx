a566b2cef9dc5392a2e82161bd749a5d7a0a2cc5
Timur Rakhimzhan <t.rakhimzhan@eyelab.kz>
2022-01-11 05:29:53 +0600

    page height fixed

--- src/shared/PageWrapper/PageWrapper.tsx
+++ src/shared/PageWrapper/PageWrapper.tsx
@@ -3,7 +3,7 @@ import {styled} from "../../themes";
 export const PageWrapperStyled = styled('div', {
     display: 'flex',
     flexDirection: 'row',
-    height: 'calc(100vh - $headerHeight)',
+    height: 'calc((var(--vh, 1vh) * 100) - $headerHeight)',
     width: '100vw',
     padding: '$md',
     justifyContent: 'center',
