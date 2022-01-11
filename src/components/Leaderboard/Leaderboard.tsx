708b8f27868c96ccb7b57ceba6d99b014ba085bd
Timur Rakhimzhan <t.rakhimzhan@eyelab.kz>
2022-01-11 13:34:15 +0600

    top 10 leaderboard title

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -7,7 +7,7 @@ import {LeaderboardProps} from "./Leaderboard.props";
 const Leaderboard: React.FC<LeaderboardProps> = (props) => {
     return <LeaderboardContainerStyled direction={'column'} {...props}>
         <Typography className={'marginBottomLg marginLeftAuto marginRightAuto'} color={'primary'} size={'xLg'} weight={'bold'}>
-            Leaderboard
+            Top 10 Leaderboard
         </Typography>
         <LeaderboardTable />
     </LeaderboardContainerStyled>
