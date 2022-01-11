e7b9e7ca056db0eca217ff0cc3679e98b981679a
Timur Rakhimzhan <t.rakhimzhan@eyelab.kz>
2022-01-11 08:04:03 +0600

    axios fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({baseURL: `http://localhost:3001/api/`});
+const axiosInstance = axios.create({baseURL: `http://${window.location.host}/api/`});
 
 export default axiosInstance;
